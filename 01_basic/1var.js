d04dacc144a17c8dfcba5ddfb3cff567bdd747da
Mustakeem2004 <149988480+Mustakeem2004@users.noreply.github.com>
2025-01-25 13:18:50 +0000

    datatypes

---
+++ 01_basic/1var.js
@@ -0,0 +1,12 @@
+console.log("malik")
+const id=1234232
+let a="Mustakeem Malik"
+var b="00malik@gmail.com"    //don't use var it does not follow scope
+console.log(a);
+let c;
+let d=null
+console.table([id,a,b,c,d])
+console.log(typeof id);
+
+
+
